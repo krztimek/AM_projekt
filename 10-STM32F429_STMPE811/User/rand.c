4376e890744ce65d9adcd35be24445473f872a93
SzymunS <szymonszozda@gmail.com>
2017-06-25 22:09:01 +0200

    separate rand

---
+++ 10-STM32F429_STMPE811/User/rand.c
@@ -0,0 +1,18 @@
+#include "rand.h"
+
+uint8_t rand_function() {
+	return tick_from_systick;
+}
+
+
+void cards_randomize(void) {
+	for (uint8_t i = 0; i < cardsSetSize; i++) {
+		uint8_t rrand = rand_function() % (cardsSetSize - i);//rand (sys tick or sth)
+		cards[i] = cardsSet[rrand];
+		cardsSet[rrand] = cardsSet[cardsSetSize - 1 - i];
+		cardsSet[cardsSetSize - 1 - i] = 0;
+	}
+	for (int i = 0; i<20; i++) {//10 ?? possible 11 cards as winer AAAA 2222 333 = 4 + 8 + 9 = 21
+		(dealer.cardset)[i] = '\0';
+		(player.cardset)[i] = '\0';
+}
---
+++ 10-STM32F429_STMPE811/User/rand.h
@@ -0,0 +1,14 @@
+#ifndef RAND_H
+#define RAND_H
+
+/*
+function that rerurn rand number
+*/
+uint8_t rand_function(void);
+
+/*
+Funkcja cards_randomize - tasowanie kart
+*/
+void cards_randomize(void);
+
+#endif
